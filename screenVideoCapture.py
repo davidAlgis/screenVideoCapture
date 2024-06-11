6d6d36d92b94857082a6233c2fd2e3a9703e1a26
david.algis <david.algis@studio-nyx.com>
2024-06-11 13:55:09 +0200

    ✨ Capture audio too and merge the results

--- screenVideoCapture.py
+++ screenVideoCapture.py
@@ -2,12 +2,26 @@ import cv2
 import mss
 import numpy
 from pynput import keyboard
+import wave
+import pyaudio
+import threading
+import subprocess
+from moviepy.editor import VideoFileClip, AudioFileClip
 
 # Set up the screen capture.
 # Adjust this to your screen resolution.
 monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}
 sct = mss.mss()
 keep_listening = True
+
+
+# Audio capture settings
+audio_format = pyaudio.paInt16
+channels = 2
+rate = 44100
+chunk = 1024
+audio_output = "outputAudio.wav"
+video_output = "outputVideo.mp4"  # The output file.
 
 
 def on_key_press(key):
@@ -26,17 +40,42 @@ def on_key_release(key):
         return False
 
 
+def capture_audio():
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format=audio_format, channels=channels,
+                        rate=rate, input=True, frames_per_buffer=chunk)
+    frames = []
+
+    while keep_listening:
+        data = stream.read(chunk)
+        frames.append(data)
+
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    wf = wave.open(audio_output, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(audio.get_sample_size(audio_format))
+    wf.setframerate(rate)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+
 # Set up the video writer.
 # You can experiment with different codecs.
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
 fps = 30.0  # Adjust this to your desired frames per second.
-out_file = "output.mp4"  # The output file.
 video_writer = cv2.VideoWriter(
-    out_file, fourcc, fps, (monitor["width"], monitor["height"]))
+    video_output, fourcc, fps, (monitor["width"], monitor["height"]))
 
 # Set up the keyboard listener.
 listener = keyboard.Listener(on_press=on_key_press, on_release=on_key_release)
 listener.start()
+
+# Start audio capture in a separate threadq
+audio_thread = threading.Thread(target=capture_audio)
+audio_thread.start()
 
 print("Capturing screen...")
 
@@ -51,8 +90,21 @@ while keep_listening:
     # Write the frame to the video file.
     video_writer.write(img_bgr)
 
+# Wait for audio capture to finish
+audio_thread.join()
+
 print("Releasing resources...")
 
 # Release the resources.
 video_writer.release()
 cv2.destroyAllWindows()
+
+
+video = VideoFileClip(video_output)
+audio = AudioFileClip(audio_output)
+
+# Set the audio of the video clip
+video = video.set_audio(audio)
+
+# Write the final output to a file
+video.write_videofile("output.mp4", codec="libx264", audio_codec="aac")
